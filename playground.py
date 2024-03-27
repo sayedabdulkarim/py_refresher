cf8ce039d9ace907c4a86f94cacbfd7d56c11dcb
unknown <sakarim9124@gmail.com>
2024-03-28 00:29:35 +0530

    updated

---
+++ playground.py
@@ -0,0 +1,13 @@
+# a = [1,2,2,3,3]
+# print(set(a))
+# print(a)
+
+class Dog():
+
+    def __init__(self, breed):
+        self.breed = breed
+
+# 
+myDog = Dog(breed= "Poekmon")
+# print(myDog.breed)
+print(type(myDog))
