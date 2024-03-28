ea4843740ea8839c1e31cbf74abf23de8bf49741
unknown <sakarim9124@gmail.com>
2024-03-28 23:55:28 +0530

    updated

--- playground.py
+++ playground.py
@@ -4,12 +4,20 @@
 
 class Dog():
 
+    species = "MAMMAL"
+    
     def __init__(self, breed, name, spots):
+        # attributes
         self.breed = breed
         self.name = name
         self.spots = spots
+        
+        #Actions/ methods 
+    def bark(self, number):
+        print("WOOOOOOF, My name is {}, and my number is {}".format(self.name, number))
 
 # 
 myDog = Dog(breed= "Poekmon", name="Tuffy", spots=2)
-print(myDog.spots)
-# print(type(myDog))
+# print(myDog.spots)
+# print(myDog.species)
+myDog.bark(12)
