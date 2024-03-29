080f93e1e7541a42f114130896d178b47b2a8679
unknown <sakarim9124@gmail.com>
2024-03-29 22:12:02 +0530

    updated

---
+++ modules_packages.py
@@ -0,0 +1,5 @@
+from colorama import init, Fore
+
+init(autoreset=True)  # Initialize Colorama and autoreset to clear the style after every print statement
+
+print(Fore.GREEN + "This text is green!")  # This will print the text in green
