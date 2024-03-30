e224b4cde5fe43ea15e73a18b1d123dfe23ef348
unknown <sakarim9124@gmail.com>
2024-03-30 11:11:31 +0530

    updated

---
+++ numpy/playground.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# myMat = [[1,2,3],[4,5,6], [7,8,9]]
+# myMat = [1,4,5,7,7,8]
+myMat = [[1,2,3, [11, 12,13]],[4,5,6], [7,8,9]]
+
+arr = np.array(myMat)
+
+print(arr)
+# print(myMat)
