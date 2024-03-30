eff07eb70ce007aca04b7692ceeb2561ed9529d6
unknown <sakarim9124@gmail.com>
2024-03-30 17:09:22 +0530

    updated

--- numpy/playground.py
+++ numpy/playground.py
@@ -1,10 +1,18 @@
 import numpy as np
 
-# myMat = [[1,2,3],[4,5,6], [7,8,9]]
-# myMat = [1,4,5,7,7,8]
-myMat = [[1,2,3, [11, 12,13]],[4,5,6], [7,8,9]]
+# # myMat = [[1,2,3],[4,5,6], [7,8,9]]
+# # myMat = [1,4,5,7,7,8]
+# myMat = [[1,2,3, [11, 12,13]],[4,5,6], [7,8,9]]
 
-arr = np.array(myMat)
+# arr = np.array(myMat)
 
-print(arr)
+# print(arr)
 # print(myMat)
+
+#################
+# arr = np.arange(0,10, 5)
+# print(arr)
+
+# arr = np.zeros(5)
+arr = np.zeros((5, 4))
+print(arr)
