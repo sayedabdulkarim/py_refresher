6cfc3da4fa1c060063f6d65d4a4bdc3b066803f9
unknown <sakarim9124@gmail.com>
2024-04-01 23:45:50 +0530

    upated

--- pandas/playground.py
+++ pandas/playground.py
@@ -1,17 +1,23 @@
 import numpy as np;
 import pandas as pd;
 
+from numpy.random import randn
+
 labels= ['a', 'b', 'c']
 my_data = [10,20,30]
 arr = np.array(my_data)
 d = { 'a': 10, 'b': 20, 'c': 30 }
+# np.random.seed(101)
 
-
-test = pd.Series(labels)
-print(test)
+# test = pd.Series(labels)
+# print(test)
 # print(
 #     labels,
 #     my_data,
 #     arr,
 #     d
 # )
+
+df = pd.DataFrame(randn(5,4), ['A', 'B', 'C', 'D', 'E'], ['W', 'X', 'Y', 'Z'])
+
+print(df)
