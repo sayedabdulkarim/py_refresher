d59f3af17f6c9aea9790eae53c47e4ead330d565
unknown <sakarim9124@gmail.com>
2024-04-02 07:53:30 +0530

    upated

--- pandas/playground.py
+++ pandas/playground.py
@@ -7,7 +7,7 @@ labels= ['a', 'b', 'c']
 my_data = [10,20,30]
 arr = np.array(my_data)
 d = { 'a': 10, 'b': 20, 'c': 30 }
-# np.random.seed(101)
+np.random.seed(101)
 
 # test = pd.Series(labels)
 # print(test)
@@ -20,4 +20,5 @@ d = { 'a': 10, 'b': 20, 'c': 30 }
 
 df = pd.DataFrame(randn(5,4), ['A', 'B', 'C', 'D', 'E'], ['W', 'X', 'Y', 'Z'])
 
-print(df)
+# print(df['Z'])
+print(df > 0)
