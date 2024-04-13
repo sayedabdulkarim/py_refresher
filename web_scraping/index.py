871eef2515b006323166410be7f5aee8e0033919
unknown <sakarim9124@gmail.com>
2024-04-13 12:27:10 +0530

    updated

--- web_scraping/index.py
+++ web_scraping/index.py
@@ -3,4 +3,9 @@ import requests;
 
 result = requests.get("https://www.example.com/")
 
-print(result.text)
+# print(result.text)
+
+import bs4;
+
+soup = bs4.BeautifulSoup(result.text, "lxml")
+print(soup)
