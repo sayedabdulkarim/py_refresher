5320243eac2ff2cddc0c50b9420ff4fed3de56fa
unknown <sakarim9124@gmail.com>
2024-04-14 12:55:54 +0530

    updated

--- image/index.py
+++ image/index.py
@@ -3,4 +3,13 @@ from PIL import Image
 mac = Image.open("../example.webp")
 
 # print(type(mac))
-print(mac.format_description)
+# print(mac.format_description)
+
+# 
+# print(mac.crop(0,0,100,100))
+# print(mac.crop((0, 0, 100, 100)))
+
+crop_mac = mac.crop((0, 0, 500, 500))
+
+# To display the cropped image
+crop_mac.show()
